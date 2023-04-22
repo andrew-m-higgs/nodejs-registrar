39a58f8268faaaec9365737a67e4b15f0065123b
Andrew Higgs <andrew.m.higgs@gmail.com>
2023-04-22 23:01:05 +0200

    Add command to list top ten holders.

---
+++ commands/top-ten.js
@@ -0,0 +1,34 @@
+import { SlashCommandBuilder } from 'discord.js';
+import * as db_functions from '../helpers/db-functions.js';
+import 'dotenv/config';
+
+export const data = new SlashCommandBuilder()
+	.setName('top-ten')
+	.setDescription('View a list of the top ten registered holders.');
+
+export async function execute(interaction, config) {
+	const content = 'Top ten registered holders for ' + config.collection_name + '.';
+	const embeds = [];
+	const fields = [];
+	let first = true;
+
+	const db = await db_functions.dbOpen();
+	const sql = 'SELECT nickname, numnfts FROM members ORDER BY numnfts DESC LIMIT 10';
+	await db.each(sql, async (err, row) => {
+		if (err) {
+			console.log('[ERROR]: running sql. (' + sql + ').');
+		} else if (first) {
+			fields.push(`**__${row.numnfts} : ${row.nickname}__**`);
+			first = false;
+		} else {
+			fields.push(`${row.numnfts} : ${row.nickname}`);
+		}
+	});
+	embeds.push({
+		type: 'rich',
+		color: 0x8261FE,
+		title: 'Top Ten Holders',
+		description: 'The Top Ten registered holders.\n\n' + fields.join('\n'),
+	});
+	await interaction.reply({ embeds: embeds });
+}
