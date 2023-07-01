450689dd142b53f576005d8a25da6c9f3639520c
Andrew Higgs <andrew.m.higgs@gmail.com>
2023-07-01 17:27:29 +0200

    Made multi-server aware.

--- commands/config-creator-wallets.js
+++ commands/config-creator-wallets.js
@@ -1,6 +1,6 @@
 import { SlashCommandBuilder } from 'discord.js';
 import * as functions from '../helpers/functions.js';
-import * as db_functions from '../helpers/db-functions.js';
+import { logMessage } from '../helpers/admin.js';
 import 'dotenv/config';
 const NoPermission = process.env.NoPermission;
 const Green = process.env.Green;
@@ -27,18 +27,14 @@ export async function execute(interaction, config) {
 		const content = 'Updating Creator Wallets for this project.';
 
 		await interaction.reply({ content: content, embeds: embeds, ephemeral: true });
-		const sql = `UPDATE config SET wallet_strings = "${wallet_strings}";`;
 		try {
-			const db = await db_functions.dbOpen();
-			await db.run(sql);
+			config.wallet_strings = wallet_strings.split(',');
+			await config.set();
 			embeds.push({
 				type: 'rich',
 				color: colourGreen,
 				title: ':white_check_mark: Creator wallets updated.',
 			});
-
-			// Update the config variable
-			config.wallet_strings = wallet_strings.split(',');
 
 			await interaction.editReply({ content: content, embeds: embeds, ephemeral: true });
 		} catch {
@@ -48,6 +44,7 @@ export async function execute(interaction, config) {
 				title: ':no_entry: There was a problem updating the creator wallets.',
 			});
 			await interaction.editReply({ content: content, embeds: embeds, ephemeral: true });
+			logMessage(config.server_id, 'ERROR', 'There was a problem updating the creator wallets.');
 		}
 
 	} else {
