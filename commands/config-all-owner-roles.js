5e1cbafb5ab51bfecf8088bdff2eb7090589fbd3
Andrew Higgs <andrew.m.higgs@gmail.com>
2023-07-01 18:18:57 +0200

    Made multi-server aware.

--- commands/config-all-owner-roles.js
+++ commands/config-all-owner-roles.js
@@ -1,6 +1,6 @@
 import { SlashCommandBuilder } from 'discord.js' ;
 import * as functions from '../helpers/functions.js';
-import * as db_functions from '../helpers/db-functions.js';
+import { logMessage } from '../helpers/admin.js';
 import 'dotenv/config';
 const NoPermission = process.env.NoPermission;
 const Green = process.env.Green;
@@ -9,14 +9,14 @@ const Red = process.env.Red;
 export const data = new SlashCommandBuilder()
 	.setName('config-all-owner-roles')
 	.setDescription('Config whether or not members get all owner roles for which they qualify or just the highest one.')
-	.addNumberOption(option =>
+	.addStringOption(option =>
 		option
 			.setName('all-owner-roles')
 			.setDescription('Config whether or not members get all owner roles for which they qualify or just the highest one.')
 			.setRequired(true)
 			.addChoices(
-				{ name: 'True', value: 0 },
-				{ name: 'False', value: 1 },
+				{ name: 'True', value: 'T' },
+				{ name: 'False', value: 'F' },
 			),
 	);
 
@@ -26,23 +26,19 @@ export async function execute(interaction, config) {
 	const colourGreen = parseInt(Green);
 
 	if (isAdmin) {
-		const all_owner_roles = await interaction.options.getNumber('all-owner-roles');
+		const all_owner_roles = await interaction.options.getString('all-owner-roles');
 		const embeds = [];
 		const content = 'Updating the all owner roles config for this project.';
 
 		await interaction.reply({ content: content, embeds: embeds, ephemeral: true });
-		const sql = `UPDATE config SET all_owner_roles = "${all_owner_roles}";`;
 		try {
-			const db = await db_functions.dbOpen();
-			await db.run(sql);
+			config.all_owner_roles = all_owner_roles;
+			await config.set();
 			embeds.push({
 				type: 'rich',
 				color: colourGreen,
 				title: ':white_check_mark: All owner roles updated.',
 			});
-
-			// Update the config variable
-			config.all_owner_roles = all_owner_roles;
 
 			await interaction.editReply({ content: content, embeds: embeds, ephemeral: true });
 		} catch {
@@ -51,6 +47,7 @@ export async function execute(interaction, config) {
 				color: colourRed,
 				title: ':no_entry: There was a problem updating the all owners role config.',
 			});
+			logMessage(config.server_id, 'ERROR', 'There was a problem updating the all owners role config.');
 			await interaction.editReply({ content: content, embeds: embeds, ephemeral: true });
 		}
 
