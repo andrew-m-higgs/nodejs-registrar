07d5ba4c90303df625e4456935db65232cbfe802
Andrew Higgs <andrew.m.higgs@gmail.com>
2023-07-01 17:38:03 +0200

    Made multi-server aware.

--- commands/config-admin-role.js
+++ commands/config-admin-role.js
@@ -1,6 +1,6 @@
 import { SlashCommandBuilder } from 'discord.js';
 import * as functions from '../helpers/functions.js';
-import * as db_functions from '../helpers/db-functions.js';
+import { logMessage } from '../helpers/admin.js';
 import 'dotenv/config';
 const NoPermission = process.env.NoPermission;
 const Green = process.env.Green;
@@ -25,25 +25,21 @@ export async function execute(interaction, config) {
 
 	if (isAdmin) {
 		const admin_role = await interaction.options.getRole('admin-role');
-		console.log('ID: ' + admin_role.id);
-		console.log('Name: ' + admin_role.name);
+		// console.log('ID: ' + admin_role.id);
+		// console.log('Name: ' + admin_role.name);
 		const embeds = [];
 		const content = 'Updating the admin role for this project.';
 
 		await interaction.reply({ content: content, embeds: embeds, ephemeral: true });
-		const sql = `UPDATE config SET admin_role_id = "${admin_role.id}", admin_role_name = "${admin_role.name}";`;
 		try {
-			const db = await db_functions.dbOpen();
-			await db.run(sql);
+			config.admin_role_id = admin_role.id;
+			config.admin_role_name = admin_role.name;
+			await config.set();
 			embeds.push({
 				type: 'rich',
 				color: colourGreen,
 				title: ':white_check_mark: The admin role has been updated.',
 			});
-
-			// Update the config variable
-			config.admin_role_id = admin_role.id;
-			config.admin_role_name = admin_role.name;
 
 			await interaction.editReply({ content: content, embeds: embeds, ephemeral: true });
 		} catch {
@@ -52,6 +48,7 @@ export async function execute(interaction, config) {
 				color: colourRed,
 				title: ':no_entry: There was a problem updating the admin role.',
 			});
+			logMessage(config.server_id, 'ERROR', 'There was a problem updating the admin role.');
 			await interaction.editReply({ content: content, embeds: embeds, ephemeral: true });
 		}
 
